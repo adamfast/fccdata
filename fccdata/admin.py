8bde6776d4805549d23f420b6a476abf2ae82c79
Adam Fast <adam@adamfast.com>
2011-08-20 11:09:46 -0500

    Include improved admin setups for the three main models.

--- fccdata/admin.py
+++ fccdata/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from fccdata.models import *
 
-admin.site.register(am)
-admin.site.register(en)
-admin.site.register(hd)
+class enAdmin(admin.ModelAdmin):
+    list_display = ('call_sign','first_name','last_name')
+    search_fields = ('call_sign',)
+
+class amAdmin(admin.ModelAdmin):
+    list_display = ('call_sign','operator_class')
+    search_fields = ('call_sign',)
+    raw_id_fields = ('unique_system_identifier',)
+
+class hdAdmin(admin.ModelAdmin):
+    list_display = ('call_sign','license_status','expired_date')
+    search_fields = ('call_sign',)
+    raw_id_fields = ('unique_system_identifier',)
+
+admin.site.register(en, enAdmin)
+admin.site.register(am, amAdmin)
+admin.site.register(hd, hdAdmin)
