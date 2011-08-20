0ba294d932bf05b7cdfc19c6e88343fdaa6cc93c
Adam Fast <adam@adamfast.com>
2011-08-20 11:09:26 -0500

    Add a setup.py to allow inclusion in requirements files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='fccdata',
+    version='1.0.0',
+    description='Models and load scripts used to import FCC data (priarmily focused on amateur radio) into Django.',
+    author='Adam Fast',
+    author_email='adamfast@gmail.com',
+    url='',
+    packages=find_packages(),
+    package_data={
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
